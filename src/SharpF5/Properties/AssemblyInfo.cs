e52720a972c46b0982bda89643b0a1fca6e16f08
nikvoronin <nvoronin@outlook.com>
2016-06-14 11:12:41 +0300

    nuget .2

--- src/SharpF5/Properties/AssemblyInfo.cs
+++ src/SharpF5/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle(".NET Ethernet Communication Library for KEB F5 Combivert")]
+[assembly: AssemblyTitle("Communication Library for KEB F5 Combiverts")]
 [assembly: AssemblyDescription("Ethernet Communication Library for .NET allows read and write registers of the F5 Combivert with help of the DIN66019-II protocol.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nikolai Voronin")]
@@ -8,4 +8,4 @@
 [assembly: AssemblyCopyright("Copyright © 2008-2015 Nikolai Voronin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.5.*")]
+[assembly: AssemblyVersion("2.5.1")]
